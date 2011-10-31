32377a1e054a8af76a6590a59b9472fb274bc2d9
Anthony Cowley <acowley@tremolo>
2011-10-31 18:24:53 -0400

    Fixed top-level model loading function.

--- Graphics/Formats/Collada.hs
+++ Graphics/Formats/Collada.hs
@@ -1,9 +1,13 @@
 module Graphics.Formats.Collada (load) where
+import Data.Maybe (listToMaybe)
 import Graphics.Formats.Collada.Objects
 import Text.XML.HXT.Core
 
 -- | Loads a COLLADA file and returns an action that draws the model.
 -- This will throw an IO exception if anything went wrong during the
 -- process.
-load :: FilePath -> IO (Model)
-load f = runX $ readDocument [] f >>> parseCollada
+load :: FilePath -> IO (Maybe Model)
+load f = fmap listToMaybe . runX $ 
+         readDocument [] f >>> getChildren >>> parseCollada
+
+
